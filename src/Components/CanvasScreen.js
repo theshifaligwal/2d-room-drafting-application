49babad636d7f732188c0c15e457376c942ccff0
Shifali Gwal <gwal.shifali2223@gmail.com>
2022-02-03 20:39:38 +0530

    Initial Commit

---
+++ src/Components/CanvasScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CanvasScreen = () => {
+  return (
+    <div>
+      <h1>CanvasScreen</h1>
+    </div>
+  );
+};
+
+export default CanvasScreen;
---
+++ src/Components/ToolBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ToolBar = () => {
+  return (
+    <div>
+      <h1>ToolBar</h1>
+    </div>
+  );
+};
+
+export default ToolBar;
